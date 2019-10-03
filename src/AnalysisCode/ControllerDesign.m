10e81a6344ee15a139a8c96e9320d180ea94e4b1
武田広大 <ltf.smh.raika@gmail.com>
2019-10-03 23:49:59 +0900

    新規解析コード

---
+++ src/AnalysisCode/ControllerDesign.m
@@ -0,0 +1,85 @@
+clear allvars;
+
+J = 0.005;  %inertia
+D = 0.026;  %Damping
+Kt = 1.2;   %Torque transform ratio
+wp = 20;    %Pole 
+Ku = 10;    %limit Kp gain
+Pu = 0.2;   %Time period at Ku
+SigNum = 3; %Significant number
+
+
+%% design PD
+[Cpd,CoPD] = designPD(Kt,J,D,wp);
+
+tau = 1/CoPD.a0;
+Kp = CoPD.b0*tau;
+Kd = (CoPD.b1-Kp)*tau;
+
+disp(strcat('PD Gain: [tau, Kp, Kd]=[', num2str(round(tau,SigNum,'significant')),',', num2str(round(Kp,SigNum,'significant')),',', num2str(round(Kd,SigNum,'significant')),']'));
+
+
+%% design PID
+
+[Cpid,CoPID] = designPID(Kt,J,D,wp);
+
+tau = 1/CoPID.a1;
+Ki = CoPID.b0*tau;
+Kp = (CoPID.b1-Ki)*tau;
+Kd = (CoPID.b2-Kp)*tau;
+
+disp(strcat('PID Gain: [tau, Kp, Kd, Ki]=[', num2str(round(tau,SigNum,'significant')),', ', num2str(round(Kp,SigNum,'significant')),',', num2str(round(Kd,SigNum,'significant')),',', num2str(round(Ki,SigNum,'significant')),']'));
+
+%% design PID by Ziegler-Nichols
+
+disp(strcat('PID Gain by ZN: [Kp, Kd, Ki]=[', num2str(round(0.5*Ku,SigNum,'significant')),',', num2str(round(2/Pu,SigNum,'significant')),',', num2str(round(Pu/8,SigNum,'significant')),']'));
+
+
+
+function [Cpd,Coeff] = designPD(Kt,J,D,wp)
+
+d = D/J;
+K = Kt/J;
+
+Spd =  [1 0 0 0;
+        d 1 0 0;
+        0 d K 0;
+        0 0 0 K;];
+
+bpd = [1; 3*wp; 3*wp^2; wp^3];
+
+PDpara = Spd\bpd;
+Coeff.a0 = PDpara(2);
+Coeff.b1 = PDpara(3);
+Coeff.b0 = PDpara(4);
+
+
+s = tf('s');
+Cpd = (Coeff.b1*s + Coeff.b0)/(s + Coeff.a0);
+
+end
+
+function [Cpid,Coeff] = designPID(Kt,J,D,wp)
+
+Kt = Kt/J;
+d = D/J;
+k = 0;
+
+Spid = [1 0 0 0 0;
+        d 1 0 0 0;
+        k d Kt 0 0;
+        0 k 0 Kt 0;
+        0 0 0 0 Kt;];
+
+bpid = [1; 4*wp; 6*wp^2; 4*wp^3; wp^4];
+
+PIDpara = Spid\bpid;
+Coeff.a1 = PIDpara(2);
+Coeff.b2 = PIDpara(3);
+Coeff.b1 = PIDpara(4);
+Coeff.b0 = PIDpara(5);
+
+s = tf('s');
+Cpid = (Coeff.b2*s^2 + Coeff.b1*s + Coeff.b0)/(s^2 + Coeff.a1*s);
+
+end
---
+++ src/AnalysisCode/sys_ident.m
@@ -0,0 +1,96 @@
+%% simulation set
+close all
+freq = 1000; %1000hz
+Ts = 1/freq; %Sampling time
+Tc = 2; %Chirp period
+Kt = 1.2; %transformation ratio from V to Nm 
+
+
+
+%load('result.data'); 
+
+list = ls('ChirpResults/*.csv'); % Check all the files with the extension .csv
+list = string(list);
+
+for Nlist = 1:length(list)
+    filedir = strcat('ChirpResults/',list(Nlist));
+    
+    %% read data and set in/out data
+
+    load(filedir); % Load file data
+
+    time = result(:,1); %time
+    in = result(:,2)*Kt; %Input: Torque 
+    out = result(:,3); %Output: Angular speed 
+
+    figure(1); plot(time,in,time,out);
+    xlabel('time[s]')
+    ylabel('in[A]/out[rad/s]')
+    legend('In','Out')
+    grid on;
+
+    %% tfestimate to get bodeplot
+    in = detrend(in,0); out = detrend(out,0);
+    [PXX,FREQ] = tfestimate(in,out,rectwin(Tc/Ts),0,Tc/Ts,freq);%rectangluar window and no overwrap
+    Pfrd = frd(PXX,FREQ,'FrequencyUnit','Hz');
+
+    
+    % bodeoptions
+    opt = bodeoptions('cstprefs');
+    opt.PhaseWrapping = 'on';
+    opt.PhaseWrappingBranch = -180;
+    %{
+    figure(2);
+    bode(Pfrd,opt);
+    title('Freq responce')
+    %}
+    
+    %% mscoherence
+    [COHER,cFREQ] = mscohere(in,out,rectwin(Tc/Ts),0,Tc/Ts,freq);
+
+    figure(3); semilogx(cFREQ,COHER);
+
+    %%%%%% from here Matlab needs SystemIdentificationToolbox %%%%%%%%%%%%%
+    %% tfest fitting part
+    Nz = 0;%Number of zeros of the plant
+    Np = 1;%Number of poles of the plant
+    fmin=0.5; %minimum freq of chirp signal 
+    fmax=50; %maximum freq of chirp signal
+    Td = 0:0.1e-3:5e-3;% DelayTime search range: 0 to 5ms with 0.1ms step
+
+    [~,kmin]=min(abs(fmin-Pfrd.freq));%search data point index corresponding to fmin
+    [~,kmax]=min(abs(fmax-Pfrd.freq));%search data point index corresponding to fmax
+
+    %tfestimation setup
+    init_sys = idtf(ones(1,Nz+1),ones(1,Np+1),'IODelay',0e-3);% Nz-order/Np-order TF with 0ms delay
+
+    % constraints for coefficnets of TF
+    init_sys.Structure.Numerator.Minimum = 0;
+    init_sys.Structure.Numerator.Maximum = inf;
+    init_sys.Structure.Denominator.Minimum = 0;
+    init_sys.Structure.Denominator.Maximum = inf;
+
+    tfopt = tfestOptions('WeightingFilter',COHER(kmin:kmax).*FREQ(kmin:kmax));%Weighting with coherence
+
+    FP = 0;%minimum fit percentage of tfest function
+
+    %Lookup function to find most likely IO delay time
+    for DT = 1:length(Td)
+        init_sys.IODelay=Td(DT);
+        Pest = tfest(fdel(Pfrd,[Pfrd.freq(1:kmin-1);Pfrd.freq(kmax+1:end);]),init_sys,tfopt);
+        if FP < Pest.Report.Fit.FitPercent
+            FP = Pest.Report.Fit.FitPercent;
+            Pest_most = Pest;
+        end
+    end
+
+    figure();
+
+    bode(Pfrd,opt,Pest_most,opt);
+    xlim([0.1 500]);
+    title(strcat('Freq responce - ',num2str(Pest_most.IODelay*1e3),'ms IODeelay'))
+
+    disp(filedir);
+    disp(strcat('J = ',num2str(1/Pest_most.Numerator),'[kgm^2], D = ', num2str(Pest_most.Denominator(2)/Pest_most.Numerator),'[kgm^2s]'))
+    
+end
