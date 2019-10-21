7c79a8b51e81d7e83585954c58fe51902a8d475d
武田広大 <ltf.smh.raika@gmail.com>
2019-10-21 21:42:51 +0900

    PIDの実装方法を修正

--- src/AdvancedControlPrograms/SimplePID/pid.c
+++ src/AdvancedControlPrograms/SimplePID/pid.c
@@ -8,45 +8,62 @@
 #include </usr/include/fbiad.h> /* iji */
 #include "/usr/src/linux-2.4.20-ART/include/linux/art_types.h" /*  iji */
 #include "/usr/src/linux-2.4.20-ART/include/linux/art_task.h" /* iji  */
+
 #define TRUE 1
 
 #define KBD_PORT 0x61
 #define SPK_BIT  0x02
 #define LP0_PORT 0x378
-#define ENCADRES0 0x9800
+
 #define ADADRES0 0x9000
 #define DAADRES0 0x9400
-#define ENC_RESL 312500
+#define ENCADRES0 0x9800
 
-int Ts,Tcon;
+#define ENC_RESL 312500.0
+
+int Tcon, Ts;
+double V_limit;
 
 /* motor constants */
 double Jn; //[kgm^2]
-double Ktn = 0.6; //[Nm/A]
-	// D/A output:-10[V]~10[V]
-	// Virtual torque current:-10[A]~10[A]
-	// corresponding torque:6 Nm
-	// 					-9.8[Nm]~9.8[Nm]
+double Ktn = 1.8; //[Nm/V]
+	//motor is converted in 2015
+	// D/A output:-5[V]~5[V]
+	// Corresponding torque:-9[Nm]~9[Nm]
+double Max_T = 9.0;
+	// Maximum torque of motor driver
 
 /* controller variables definition */
-double I_ref=0.0, I_limit;
+double T_ref=0.0;
 double X, X_ref;
-double u_x, u, uxn_1=0.0, iu=0.0, iu_1=0.0, u_1=0.0, du;
-double Tr, Zt, Kp, Kd, Ki;
+//double u_x, u, uxn_1=0.0, iu=0.0, iu_1=0.0, u_1=0.0, du;
+double Kp, Kd, Ki;
 double Tau;
 double T_smpl=0.0;
-double b0,b1,b2;
-double c1, c2, cu;
-double y,ddX;
-double I01, I01_1=0.0, I0n,I0n_1=0.0;
-double x1=0.0,x1n=0.0,x2=0.0,x1n_1=0.0,x2n=0.0,x2n_1=0.0, x2n_2=0.0;
+double a1, b0, c0;
+//double c1, c2, cu;
+double dX,ddX;
+//double I01, I01_1=0.0, I0n,I0n_1=0.0;
+//double x1=0.0,x1n=0.0,x2=0.0,x1n_1=0.0,x2n=0.0,x2n_1=0.0, x2n_2=0.0;
+double Vn=0.0, Vn_1=0.0, V1=0.0, V1_1=0.0, V2=0.0, V2_1=0.0, V0;
 /*----------------------------------------------*/
-double control(double X_r,double Xs)
+double control(double Xref,double Xmeas)
 {
+/* V0 */
+	dX = Xref - Xmeas; 
 
-/* IO */
-	
-	u = X_r - Xs; 
+	Vn = dX - a1*V1_1;
+	V1 = V1_1 + (Vn+Vn_1)*T_smpl/2.0;
+	V2 = V2_1 + (V1+V1_1)*T_smpl/2.0;
+
+	V0 = c0*dX - b0*V2;
+
+	Vn = Vn_1;
+	V1 = V1_1;
+	V2 = V2_1;
+
+/*
+	u = Xref - Xmeas; 
 
 	u_x = u - x2n_1/Tau;
 	x2n = x2n_1 + (u_x+uxn_1)*T_smpl/2.0;
@@ -54,25 +71,29 @@ double control(double X_r,double Xs)
 
 	iu = iu_1 + (u+u_1)*T_smpl/2.0;
 	
-	y=Ki*iu + Kd*du + Kp*u;
+	V0=Ki*iu + Kd*du + Kp*u;
 
 	uxn_1 = u_x;
 	u_1 = u;
 	iu_1 = iu;
 	x2n_1=x2n;
+*/
 
-/* I_ref */
-	return y;
+/* Vout */
+	return V0;
 }
 /*----------------------------------------------*/
-/* encorder initialize */
+
+///////////* encorder initialize *////////////
 int enc_init(){
 	fprintf(stderr,"Encoder ADRESS: %4x\n",ENCADRES0);
 	outb(0x6,ENCADRES0 + 0x04);
 	outb(0x6,ENCADRES0 + 0x14);
 	return 0;
 }
-/* Ana->Dig transfer */
+//////////////////////////////////////////////
+
+///////////* Ana->Dig transfer *//////////////
 static double Adtransfer(int ch)
 {
 unsigned short SmplAd=0;
@@ -83,7 +104,6 @@ unsigned char lmb,hmb;
 	while(inb(ADADRES0+0x3)!=0x80){}
 	lmb=inb(ADADRES0);
 	hmb=inb(ADADRES0+0x1)-0x80;
-
 	SmplAd = hmb*256+lmb;
 	break;
 	case 2:
@@ -138,20 +158,22 @@ unsigned char lmb,hmb;
 	}
 	return (double)(20.0*SmplAd)/4096.0-10.0;
 }
-/* Dig->Ana transfer */
+//////////////////////////////////////////////
+
+///////////* Dig->Ana transfer *//////////////
 static int Datransfer(int ch,double DaVout)
 {
 	unsigned char lmb,hmb;
 	unsigned short SmplDa;
 	if(DaVout>=5.0){
-	SmplDa = 4095;
+		SmplDa = 4095;
 	}
 	else{
 		if(DaVout<=-5.0){
-		SmplDa = 0;
+			SmplDa = 0;
 		}
 		else{
-		SmplDa = DaVout*4096.0/10.0+2048;
+			SmplDa = DaVout*4096.0/10.0+2048;
 		}
 	}
 	switch(ch){
@@ -193,8 +215,10 @@ static int Datransfer(int ch,double DaVout)
 	}
 	return 0;
 }
+//////////////////////////////////////////////
 
-/* read encorder */
+
+/////////////* read encorder *////////////////
 static double read_theta(int ch)
 {
 	int cnt;
@@ -203,15 +227,15 @@ static double read_theta(int ch)
 	unsigned int adr;
 
 	switch(ch){
-	case 1:
-	adr = ENCADRES0;
-	break;
-	case 2:
-	adr = ENCADRES0 + 0x10;
-	break;
-	default:
-	adr=0;
-	break;
+		case 1:
+			adr = ENCADRES0;
+			break;
+		case 2:
+			adr = ENCADRES0 + 0x10;
+			break;
+		default:
+			adr=0;
+			break;
 	}
 
 	outb(0x2,ENCADRES0 + 0x06);
@@ -221,116 +245,143 @@ static double read_theta(int ch)
 	hbyt=inb(adr+0x3);
 	cnt = hbyt*16777216+m2byt*65536+m1byt*256+lbyt;
 	rad=(double)cnt*(-2.0)*M_PI/ENC_RESL;
+	//motor is converted to new version in 2015
 	return(rad);
 }
+//////////////////////////////////////////////
 
-/* real time stop */
+/////////////* real time stop *///////////////
 void ctlstop(){
-unsigned char 	OutChar;
+	unsigned char 	OutChar;
 	OutChar=0;
 	outb(OutChar, LP0_PORT);
 	fprintf(stderr,"motor-stop\n");
-	
 	Datransfer(1,0.0);
 
 	if(DaClose(1)==-1||AdClose(1)){
-	fprintf(stderr,"iopl failed\n");
-	exit(1);
+		fprintf(stderr,"iopl failed\n");
+		exit(1);
 	}
 
-      if (art_exit() == -1) {
-          perror("art_exit");
-          exit(1);
-      }
+	if (art_exit() == -1) {
+		perror("art_exit");
+		exit(1);
+	}
 
 	return;
 }
+//////////////////////////////////////////////
 
+
+
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//						MAIN PROGRAM 						// 
+//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
-int i, cntnum, beep;
-unsigned char 	OutChar=0;
-double theta1, Data, Vout;
-int res,dnum;
-unsigned long ulpNum;
-ADSMPLREQ Smplreq;
-DASMPLREQ Conf;
-FILE *resfile;
-double *tmpDataI,*tmpDataXref,*tmpDataX;
+	int i, cntnum, beep;
+	unsigned char 	OutChar=0;
+	double ExtRef, Vout;
+	int res,dnum;
+	unsigned long ulpNum;
+	ADSMPLREQ Smplreq;
+	DASMPLREQ Conf;
+	FILE *resfile;
+	double *tmpDataV,*tmpDataXref,*tmpDataX;
 
-/* sampling time */
+	///////////////////////////////////////////////
+	////////////////* sampling time *//////////////
+	///////////////////////////////////////////////
 	Ts=0;
-	while(Ts == 0){
-	printf("\n sampling time [msec] (1msec):");
-	scanf("%d",&Ts);
+	while(Ts <= 0){
+		printf("\n sampling time [us] (1000 us):");
+		scanf("%d",&Ts);
 	}
-	Ts=Ts*1000;
+	
+	///////////////////////////////////////////////
+	////////////////* current limit *//////////////
+	///////////////////////////////////////////////
+	V_limit=-1;
+	while(V_limit < 0 && V_limit > Max_T){
+		printf("\n Torque limit [Nm] (9.0 Nm) :");
+		scanf("%lf",&V_limit);
+	}
+	V_limit/=Ktn; 
 
-/* current limit */
-	printf("\n I_limit (current limit [A]) (5.0A) :");
-	scanf("%lf",&I_limit);
-
-/* control variables */
-	printf("\n Jn (nominal inertia [kgm2]) (0.04) :");
-	scanf("%lf",&Jn);
-	//printf("\n Tr (position response [ms]) (50ms) :");
-	//scanf("%lf",&Tr);
-	//Tr/=1000.0;
-	//printf("\n Zt (damping factor) (1.0) :");
-	//scanf("%lf",&Zt);
-	printf("\n Kp :");
+	///////////////////////////////////////////////
+	////////////* control variables *//////////////
+	///////////////////////////////////////////////
+	printf("\n Kp (propotional gain) :");
 	scanf("%lf",&Kp);
-	printf("\n Ki :");
-	scanf("%lf",&Ki);
-	printf("\n Kd :");
+	printf("\n Kd (derivative gain) :");
 	scanf("%lf",&Kd);
-	printf("\n Tau (derivative calq. [ms]) (5ms) :");
-	scanf("%lf",&Tau);
+	printf("\n Ki (integrate gain) :");
+	scanf("%lf",&Ki);
+	Tau=-1;
+	while(Tau<=0){
+		printf("\n Tau (Low pass filter time constant [ms]) (5ms) :");
+		scanf("%lf",&Tau);
+	}
 	Tau /=1000.0;
-	b0=Ki;
-	b1=Kp+Ki*Tau;
-	b2=Kp*Tau+Kd;
-	c1=b0/Tau;
-	c2=b1/Tau-b2/Tau/Tau;
-	cu=b2/Tau;
+	a1=1.0/Tau;
+	b0=Ki*Kd/Tau/Tau;
+	c0=Kp+Kd/Tau;
+	
 
-/* printout of the control parameters */
-	printf("\n motor constants :\n Jn = %f [kgm2], Ktn = %f [Nm/A]",Jn,Ktn);
-	printf("\n position controller gain :\n Kp = %f [A/rad], Kd = %f [A/(rad/s)]",Kp,Kd);
+	///////////////////////////////////////////////
+	/////* printout of the control parameters *////
+	///////////////////////////////////////////////
+	printf("\n motor constants :\n Ktn = %f [Nm/V]" ,Ktn);
+	printf("\n position controller gain :\n Kp = %f [V/rad], Kd = %f [V/(rad/s)]",Kp,Kd);
 
-/* set control loop counter and start control */
-	printf("\n \n Operation Time [s] :");
-	scanf("%d",&Tcon);
+	///////////////////////////////////////////////
+	//set control loop counter and start control///
+	///////////////////////////////////////////////
+	Tcon = -1;
+	while(Tcon < 0){
+		printf("\n \n Operation Time [s] :");
+		scanf("%d",&Tcon);
+	}
 	Tcon = Tcon*1000000/Ts;
-	tmpDataI=calloc(Tcon, sizeof(double));
+
+	tmpDataV=calloc(Tcon, sizeof(double));
 	tmpDataX=calloc(Tcon, sizeof(double));
 	tmpDataXref=calloc(Tcon, sizeof(double));
 
-/* boad open ,initialize */	
+	////////////////////////////////////////////
+	//////* boad open ,initialize */////////////
+	////////////////////////////////////////////
 	if(DaOpen(1)==-1||AdOpen(1)==-1){
-	fprintf(stderr,"DA Open(1) failed\n");
-	exit(1);
+		fprintf(stderr,"DA Open(1) failed\n");
+		exit(1);
 	}
 
+	// I/O permission change 
 	if(iopl(3)==-1){
-	fprintf(stderr,"iopl failed\n");
-	exit(1);
+		fprintf(stderr,"iopl failed\n");
+		exit(1);
 	}
 
 	enc_init();
 	cntnum=0;
 	signal(SIGINT,ctlstop);
-	Vout=0.0;
 
-/* real time system open */
+	////////////////////////////////////////////
+	//////* real time system open */////////////
+	////////////////////////////////////////////
       
-      if (art_enter(ART_PRIO_MAX, ART_TASK_PERIODIC, Ts) == -1) {
-          perror("art_enter");
-          exit(1);
-      }
+	if (art_enter(ART_PRIO_MAX, ART_TASK_PERIODIC, Ts) == -1) {
+		perror("art_enter");
+		exit(1);
+	}
 	T_smpl=(double)Ts/1000000;
 
-/* DA open,initialize */
+	////////////////////////////////////////////
+	//////* DA open, initialize *///////////////
+	////////////////////////////////////////////
+
 	dnum=1;
 
 	res = DaGetSamplingConfig(dnum, &Conf);
@@ -348,6 +399,7 @@ double *tmpDataI,*tmpDataXref,*tmpDataX;
 	Conf.ulSamplingMode = DA_IO_SAMPLING;
 	Conf.ulSmplRepeat = 1;
 	Conf.fSmplFreq = 100000.0;
+
 	res = DaSetSamplingConfig(dnum, &Conf);
 	if(res){
 		printf("DaSetSamplingConfig error: res=%x\n", res);
@@ -362,7 +414,10 @@ double *tmpDataI,*tmpDataXref,*tmpDataX;
 		exit(EXIT_FAILURE);
 	}
 
-/* AD open, initialize */
+	////////////////////////////////////////////
+	//////* AD open, initialize *///////////////
+	////////////////////////////////////////////
+
 	memset(&Smplreq, 0, sizeof(ADSMPLREQ));
 	res = AdGetSamplingConfig(1,&Smplreq);
 	if(res){
@@ -391,79 +446,83 @@ double *tmpDataI,*tmpDataXref,*tmpDataX;
 		AdClose(dnum);DaClose(dnum);
 		exit(EXIT_FAILURE);
 	}
-/* real time system start */
-
+	
+	////////////////////////////////////////////
+	//////* real time system start *////////////
+	////////////////////////////////////////////
 	for (i = 0; i < Tcon; ++i) {
 
-	if(OutChar==1){
-	OutChar=0;
-	outb(OutChar, LP0_PORT);
-	}
-	else{
-	OutChar=1;
-	outb(OutChar, LP0_PORT);
-	}
+		if(OutChar==1){
+			OutChar=0;
+			outb(OutChar, LP0_PORT);
+		}
+		else{
+			OutChar=1;
+			outb(OutChar, LP0_PORT);
+		}
 
-	theta1=read_theta(1);
+		X=read_theta(1);//measured position[rad]
+		ExtRef=Adtransfer(1);//reference voltage[V]
+		X_ref=ExtRef*(1.0-exp(-T_smpl*(double)i));//reference position[rad]: V -> rad
+		T_ref=control(X_ref,X);
+		Vout=T_ref; //Torque -> Voltage reference
+
+		if(Vout>=V_limit) Vout = V_limit;
+		if(Vout<=-V_limit) Vout = -V_limit;
+
+		Datransfer(1,Vout);
+		Datransfer(2,X);
+		
+		tmpDataV[i]=T_ref;
+		tmpDataXref[i]=X_ref;
+		tmpDataX[i]=X;
+
+		//stop program during sampling time
+		if (art_wait() == -1) {
+			Datransfer(1,0.0);
+			outb(0, LP0_PORT);
+			free(tmpDataV);
+			free(tmpDataX);
+			free(tmpDataXref);
+
+			perror("art_wait");
+			exit(1);
+		}
 	
-	Data=Adtransfer(1);
-	
-	X_ref=Data*(1.0-exp(-T_smpl*(double)i));
-	//	X_ref=Data;
-	X=theta1;
-	I_ref=control(X_ref,X);
-	Vout=I_ref;
-//	if(I_ref>=I_limit) Vout = I_limit;
-//	if(I_ref<= -I_limit) Vout = -I_limit;
-//20151006  add limitation on the current 
-	if(I_ref>=I_limit) Vout = I_ref = I_limit;
-	if(I_ref<= -I_limit) Vout = I_ref = -I_limit;
-	Datransfer(1,Vout);
-
-	tmpDataI[i]=I_ref;
-	tmpDataXref[i]=X_ref;
-	tmpDataX[i]=X;
-
-         if (art_wait() == -1) {
-		Datransfer(1,0.0);
-		outb(0, LP0_PORT);
-		free(tmpDataI);
-		free(tmpDataX);
-		free(tmpDataXref);
-
-		perror("art_wait");
-		exit(1);
-          }
-	Datransfer(2,X);
 	}
+
 	Datransfer(1,0.0);
-	resfile=fopen("result_PID.csv","w+");
+	resfile=fopen("result_PD.csv","w+");
+	printf("\n File format: Time, Voltage(Torque), Postion(reference), Position(measured)\n");
 	for(i=0;i<Tcon;i++){
-	fprintf(resfile,"%f %f %f %f\n",i*T_smpl,tmpDataI[i],tmpDataXref[i],tmpDataX[i]);
+		fprintf(resfile,"%f %f %f %f\n",i*T_smpl,tmpDataV[i],tmpDataXref[i],tmpDataX[i]);
+		//File format: Time, Voltage(Torque), Postion(reference), Position(measured)
 	}
 	fclose(resfile);
 
-        /////////////////////////////////////////////
-        ///   BEEP SOUND ////////////////////////////
-        /////////////////////////////////////////////
+	free(tmpDataV);
+	free(tmpDataX);
+	free(tmpDataXref);
 
+	/////////////////////////////////////////////
+	///   BEEP SOUND ////////////////////////////
+	/////////////////////////////////////////////
 	for(i=0;i<600;i++){
-	  if(art_wait()==-1){
-		perror("art_wait");
-		exit(1);
-	  }
-	  beep=inb(KBD_PORT);
-	  beep=(i&1)? beep | SPK_BIT : beep & ~SPK_BIT;
-	  outb(beep,KBD_PORT);
+		if(art_wait()==-1){
+			perror("art_wait");
+			exit(1);
+		}
+		beep=inb(KBD_PORT);
+		beep=(i&1)? beep | SPK_BIT : beep & ~SPK_BIT;
+		outb(beep,KBD_PORT);
 	}
 	/////////////////////////////////////////////
 
-	free(tmpDataI);
-	free(tmpDataX);
-	free(tmpDataXref);
-/* real time system stop */
+
+	/////////////////////////////////////////////
+	///////* real time system stop */////////////
+	/////////////////////////////////////////////
 	ctlstop();
 
-
-      return 0;
+	return 0;
   }
